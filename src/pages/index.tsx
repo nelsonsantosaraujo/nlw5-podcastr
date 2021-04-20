010290d5eb8de1f783d3b8daca5644fe03b8f56e
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-04-20 10:32:52 -0300

    aula 02

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { GetStaticProps } from 'next';
+
+export default function Home(props) {
+  return (
+    <>
+      <h1>Index</h1>
+      <p>
+        { JSON.stringify(props.episodes) }
+      </p>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // 8h
+  }
+}
